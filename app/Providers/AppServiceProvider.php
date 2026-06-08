0d1b263e628c2bc390136de065890a49d6a86072
hilmianaufal <hilmiannaufal72@gmail.com>
2026-06-08 23:27:24 +0700

    oh yaya

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,26 +2,24 @@
 
 namespace App\Providers;
 
+use App\Models\Setting;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
-    use App\Models\Setting;
-    use Illuminate\Support\Facades\View;
+
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
-
-
     public function boot(): void
     {
-        View::share('setting', Setting::first());
+        if (Schema::hasTable('settings')) {
+            View::share('setting', Setting::first());
+        } else {
+            View::share('setting', null);
+        }
     }
 }
