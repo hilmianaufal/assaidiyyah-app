eaf89addeada8d9608efa5084800c7c0c99c11bd
hilmianaufal <hilmiannaufal72@gmail.com>
2026-06-09 00:33:25 +0700

    sdbjhabdhj

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -35,6 +35,7 @@
     @endif
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </head>
 
 <body class="bg-slate-50">
